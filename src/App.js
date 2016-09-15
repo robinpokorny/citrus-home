822904402bca3a22b584c967d501f0c5a7aeadf9
Robin Pokorný <me@robinpokorny.com>
2016-09-15 09:57:19 +0200

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Citrus Home</h1>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <span className="glyphicon glyphicon-search"></span>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
